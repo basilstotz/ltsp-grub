ef7bd6f261a680a28e680b0c160272bc547628bb
Basil Stotz <basil.stotz@gmail.com>
2014-03-29 21:52:15 +0100

    removed ldap stuff

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-cp usr/bin/* /usr/bin/.
 
+# copy files
+cp usr/bin/* /usr/bin/.
 cp etc/init.d/* /etc/init.d/.
+cp etc/lts.conf /etc/.
+
+# sysvinit
 update-rc.d ltsp-localsetup defaults
 
+# install apache2
 apt-get --yes install apache2
 cp etc/apache2/conf.d/* /etc/apache2/conf.d/.
 service apache2 restart
-
-cp etc/lts.conf /etc/lts.conf
-
-cp etc/adduser.conf /etc/adduser.conf
 
 exit
