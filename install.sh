db5c2a8455fbe8d458841bfc02ea5a79d6b41780
root <root@sub.(none)>
2014-04-03 15:35:27 +0200

    minor corr

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 # copy files
 cp usr/bin/* /usr/bin/.
 cp etc/init.d/* /etc/init.d/.
-cp etc/lts.conf /etc/.
+#cp etc/lts.conf /etc/.
 
 # sysvinit
-update-rc.d ltsp-localsetup defaults
+#update-rc.d ltsp-localsetup defaults
 
 # install apache2
-apt-get --yes install apache2
-cp etc/apache2/conf.d/* /etc/apache2/conf.d/.
-service apache2 restart
+#apt-get --yes install apache2
+#cp etc/apache2/conf.d/* /etc/apache2/conf.d/.
+#service apache2 restart
 
 exit
